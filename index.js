cf615950059d0fe98df7e11e8bcf4331438e2cfe
Manoel121001 <113386618+Manoel121001@users.noreply.github.com>
2024-05-30 01:39:32 -0300

    Add files via upload
    O projeto desenvolvido é uma calculadora baseada em entrada de strings, que permite ao usuário inserir expressões matemáticas diretamente via teclado. A calculadora valida a entrada do usuário após sua confirmação, realiza o cálculo da expressão inserida e apresenta o resultado. Primeiro, pensei qual seria a forma que implementaria o código. Conforme fui escrevendo, fiquei analisando o comportamento com as entradas, dessa forma, pude ter mais clareza das melhoras que teria o projeto inicial, reparando erros e entradas que seriam problemáticas. Após isso, tive a ideia da estilização. Por gosto pessoal, optei por algo mais minimalista e agradável, tentando sempre estar próximo de uma calculadora real. Ainda assim, não estava da forma que eu acreditava ser a melhor, então fiz algumas modificações, mas mantendo a ideia do CSS. Agora, nessa mudança, os resultados ficam abaixo da entrada e com melhor organização.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const input = document.querySelector("input");
-const div = document.querySelector("div");
+const resultsDiv = document.querySelector("#results");
 
-const operadores =[
+const operadores = [
     '-',
     '+',
     '*',
@@ -9,60 +9,58 @@ const operadores =[
     '%',
 ];
 
-input.addEventListener('input',(e)=>{
-    input.setAttribute('placeholder', 'Insira via teclado o calculo desejado.')
-    input.value= e.target.value.replace(/[^\d-*/+%]+/g,'') //Expressão regular, para permitir apenas caracteres desejados
-})
+input.addEventListener('input', (e) => {
+    input.setAttribute('placeholder', 'Insira via teclado o calculo desejado.');
+    input.value = e.target.value.replace(/[^\d-*/+%]+/g, ''); // Expressão regular, para permitir apenas caracteres desejados
+});
 
-input.addEventListener('keyup', (e)=> {   //Para finalizar a entrada ao apertar o ENTER no teclado
-    if(e.code == 'Enter'){
-        try{
-        const calculo = calc(input.value);
-        apresentar(calculo);
-        }catch(err){
-            //Retorna Operação invalida tenha dois caracteres de operação seguidos Ex: 10--, com o remover ele ignora o 1 e no 2 ele barra a entrada
-        console.log("Erro: ", err);
-        input.value='';
-        input.setAttribute('placeholder','Operação inválida')
+input.addEventListener('keyup', (e) => {   
+    if (e.code == 'Enter') {
+        try {
+            const calculo = calc(input.value);
+            apresentar(calculo);
+        } catch (err) {
+            // Retorna Operação invalida quando tem dois caracteres de operação seguidos Ex: 10--, com o remover ele ignora o 1 e no 2 ele barra a entrada
+            console.log("Erro: ", err);
+            input.value = '';
+            input.setAttribute('placeholder', 'Operação inválida');
         }
     }
-})
+});
 
-function calc(entrada){
+function calc(entrada) {
     entrada = remover(entrada);
-    return{
+    return {
         operacao: entrada,
         result: eval(entrada) // utilizando do eval ele executa a string como um código e retorna um objeto
     }
 }
-function apresentar(calc){ 
-     //Nessa função do código iremos deixar a apresentação do cálculo melhor, espaçando os caracteres.
+
+function apresentar(calc) {
+    // nessa função do código iremos deixar a apresentação do cálculo melhor, espaçando os caracteres.
     const span = document.createElement('span');
+    span.classList.add('result'); 
 
     let calcForm = calc.operacao;
-    for(let i=0; i<operadores.length;i++){
-        let arrayDeCaracteres = calcForm.split(operadores[i]) //Quebra a string transformando em array dos caracteres
-        calcForm=arrayDeCaracteres.join(` ${operadores[i]} `);  //join pega o array e o transforma em string
+    for (let i = 0; i < operadores.length; i++) {
+        let arrayDeCaracteres = calcForm.split(operadores[i]); // quebra a string transformando em array dos caracteres
+        calcForm = arrayDeCaracteres.join(` ${operadores[i]} `);  // join pega o array e o transforma em string
     }
 
     span.innerHTML = `
     ${calcForm} = <span>
-     
     ${(calc.result)}
     </span>
     `;
-    div.insertBefore(span,div.firstChild);
-
+    resultsDiv.insertBefore(span, resultsDiv.firstChild); 
     input.value = calc.result;
-
 }
 
-
-function remover(entrada){
-    //Nessa função, soluciona um erro, caso tenha uma entrada com ultimo caracter como operador matemático, ele a ignora. 
-  const caracteres = entrada.split("");
-  if(operadores.filter(x=>x.includes(caracteres.at(-1))).length > 0){
-    return entrada.substring(0,entrada.length - 1);
-  }
-  return entrada;
+function remover(entrada) {
+    // nessa função, soluciona um erro, caso tenha uma entrada com ultimo caracter como operador matemático, ele a ignora. 
+    const caracteres = entrada.split("");
+    if (operadores.filter(x => x.includes(caracteres.at(-1))).length > 0) {
+        return entrada.substring(0, entrada.length - 1);
+    }
+    return entrada;
 }
